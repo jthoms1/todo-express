0050fd52bb43d8ce3e011f1e29ca9c9cfa93acd1
Joshua Thomas <jthoms1@gmail.com>
2013-09-03 00:40:41 -0500

    Removed specific settings from configuration, package.json, and server.js.

---
+++ config.js
@@ -0,0 +1,34 @@
+var currentEnv = process.env.NODE_ENV || 'development';
+
+// Set current envirnoment
+exports.env = {
+  production: false,
+  staging: false,
+  development: false
+};
+
+exports.env[currentEnv] = true;
+
+exports.db = {
+  database : "",
+  protocol : "postgres",
+  host     : "",
+  port     : "5432",
+  user     : "",
+  password : "",
+  query    : {
+	  debug : true,
+	  pool  : true
+  }
+};
+
+exports.public_dir = (exports.env.development) ? "public_dev" : "public_prod";
+exports.upload_dir = "uploads";
+exports.port = process.env.PORT || 1337;
+
+/* We can access nodejitsu enviroment variables from process.env */
+/* Note: the SUBDOMAIN variable will always be defined for a nodejitsu app */
+exports.url = 'http://localhost:' + exports.port + '/';
+if (process.env.SUBDOMAIN) {
+  exports.url = 'http://' + process.env.SUBDOMAIN + '.jit.su/';
+}
---
+++ server.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
+var config = require('./config'),
+  express  = require('express'),
+  app      = express(),
+  server   = require('http').createServer(app),
+  orm      = require('orm'),
+  path     = require('path');
+
+app.configure(function () {
+  app.set('port', config.port);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'html');
+  app.engine('html', require('ejs').__express);
+  app.use(express.compress());
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('todo-express-cookie'));
+  app.use(express.cookieSession());
+  app.use(orm.express(config.db, {
+    define: function (db, models) {
+      require('./models')(db, models);
+    }
+  }));
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, config.public_dir)));
+});
+
+app.configure('development', function () {
+  app.use(express.errorHandler());
+});
+
+// Routes for the app
+require('./routes')(app);
+
+//Start the loop
+server.listen(app.get('port'), function () {
+  console.log("Express server listening on port " + app.get('port'));
+  console.log(config.url);
+});
+
+process.on('uncaughtException', function (err) {
+  console.log('Caught exception: ' + err);
+});
